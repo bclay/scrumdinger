031e71500858fb51150c3b3a198446233a0fda21
bclay <bclaypoole@gmail.com>
2024-11-05 20:54:44 -0800

    [SUI3] Create DetailEditView

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,28 @@
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+            }
+            HStack {
+                Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                    Text("Length")
+                }
+                Spacer()
+                Text("\(scrum.lengthInMinutes) minutes")
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
