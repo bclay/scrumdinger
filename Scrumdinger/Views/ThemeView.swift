c335542371f093f5ae2905cbd13ac32b75c535ec
bclay <bclaypoole@gmail.com>
2024-11-09 09:51:39 -0800

    [SUI4] Refactor views into group

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Brynn Claypoole on 11/9/24.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeView()
+}
