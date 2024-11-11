088061ebbbd9cd54a40b2e581d41b7f171c897ef
bclay <bclaypoole@gmail.com>
2024-11-10 19:29:59 -0800

    [SUI4] Add ThemeView and gitignore

--- Scrumdinger/Views/ThemeView.swift
+++ Scrumdinger/Views/ThemeView.swift
@@ -1,18 +1,22 @@
-//
-//  ThemeView.swift
-//  Scrumdinger
-//
-//  Created by Brynn Claypoole on 11/9/24.
-//
+/*
+ See LICENSE folder for this sample’s licensing information.
+ */
 
 import SwiftUI
 
 struct ThemeView: View {
+    let theme: Theme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
     }
 }
 
 #Preview {
-    ThemeView()
+    ThemeView(theme: .buttercup)
 }
